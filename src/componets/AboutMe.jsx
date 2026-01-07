b1936f1c84cde15a9cb9e45fa15ae85637f23c9c
HectorFernandezRico <hectorfernandezrico@gmail.com>
2026-01-07 14:21:27 +0100

    Si

--- src/componets/AboutMe.jsx
+++ src/componets/AboutMe.jsx
@@ -96,7 +96,7 @@ function AboutMe() {
           </div>
         </div>
         <div className="about-actions">
-          <a href="./CVHectorFernandezRico.pdf" download="CVHectorFernandezRico.pdf" className="btn btn-cv">
+          <a href={import.meta.env.BASE_URL + "CVHectorFernandezRico.pdf"} download="CVHectorFernandezRico.pdf" className="btn btn-cv">
             Descargar CV
           </a>
         </div>
